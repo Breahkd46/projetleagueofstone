7d9e44afb00a89991731d1592be0d8474e64985c
valentin.montal <valentin.montal4@gmail.com>
2019-01-21 12:52:02 +0100

    Rectif Game

--- leagueofstone/src/Game.js
+++ leagueofstone/src/Game.js
@@ -25,11 +25,7 @@ class Game extends Component {
       return (
         <div className="App">
           <header className="App-header">
-            <ul className="menu">
-              <li><a href="#compte">Compte</a></li>
-              <li><a href="#cartes">Liste de cartes</a></li>
-              <li><a href="/logout">Se déconnecter</a></li>
-            </ul>
+
             <img src={logo} className="App-logo" alt="logo" />
             <h2>League of Stones</h2>
             <p>Bienvenue</p> <br></br>
@@ -44,6 +40,11 @@ class Game extends Component {
       return (
         <div className="App">
           <header className="App-header">
+            <ul className="menu">
+              <li><a href="#compte">Compte</a></li>
+              <li><a href="#cartes">Liste de cartes</a></li>
+              <li><a href="/logout">Se déconnecter</a></li>
+            </ul>
             <Logout />
             <img src={logo} className="App-logo" alt="logo" />
             <h2>League of Stones</h2>
