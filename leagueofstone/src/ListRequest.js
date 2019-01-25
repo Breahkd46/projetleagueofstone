6e104aa84698ba583cdd961df8650b93227c9a8d
valentin.montal <valentin.montal4@gmail.com>
2019-01-25 14:18:11 +0100

    Modif requete paticipate

--- leagueofstone/src/ListRequest.js
+++ leagueofstone/src/ListRequest.js
@@ -17,34 +17,34 @@ class ListRequest extends Component {
 
     this.handleAcceptRequest = this.handleAcceptRequest.bind(this);
   }
-  componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.reloadRequests(),
-      RELOAD_TIME
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  reloadRequests() {
-    axios
-      .get(
-        SERVER_URL + "/matchmaking/participate?token=" +
-        this.props.sessionToken.token
-      )
-      .then(res => {
-        if (res.data.status === "ok") {
-          console.log(res.data.data.request);
-          if (res.data.data.request !== this.props.matchmaking.request) {
-            this.props.updateMatchmakingRequest(res.data.data.request)
-          }
-        } else {
-          console.log(res.data.message);
-        }
-      });
-  }
+  // componentDidMount() {
+  //   this.intervalID = setInterval(
+  //     () => this.reloadRequests(),
+  //     RELOAD_TIME
+  //   );
+  // }
+  //
+  // componentWillUnmount() {
+  //   clearInterval(this.intervalID);
+  // }
+  //
+  // reloadRequests() {
+  //   axios
+  //     .get(
+  //       SERVER_URL + "/matchmaking/participate?token=" +
+  //       this.props.sessionToken.token
+  //     )
+  //     .then(res => {
+  //       if (res.data.status === "ok") {
+  //         console.log(res.data.data.request);
+  //         if (res.data.data.request !== this.props.matchmaking.request) {
+  //           this.props.updateMatchmakingRequest(res.data.data.request)
+  //         }
+  //       } else {
+  //         console.log(res.data.message);
+  //       }
+  //     });
+  // }
 
   handleAcceptRequest(e) {
     axios
@@ -65,7 +65,7 @@ class ListRequest extends Component {
   }
 
   render() {
-    console.log(this.props.matchmaking.request);
+    // console.log(this.props.matchmaking.request);
     return (
       <div>
       <table>
--- leagueofstone/src/Participate.js
+++ leagueofstone/src/Participate.js
@@ -8,12 +8,15 @@ import ListRequest from './ListRequest';
 // Redux
 import { connect } from 'react-redux';
 import setMatchmaking from './actions/setMatchmaking';
+import updateMatchmakingRequest from "./actions/updateMatchmakingRequest";
+
 // import setMatch from './actions/setMatch';
 
 // Requete Server
 import axios from "axios";
 import { SERVER_URL } from "./consts";
 import { RELOAD_TIME } from "./consts";
+
 
 class Participate extends Component {
 
@@ -37,8 +40,15 @@ class Participate extends Component {
         )
         .then(res => {
           if (res.data.status === "ok") {
+            console.log(res.data.data.request)
+            console.log(this.props.matchmaking.request)
+            console.log(res.data.data.request !== this.props.matchmaking.request)
+            if (res.data.data.request !== this.props.matchmaking.request) {
+              this.props.updateMatchmakingRequest(res.data.data.request)
+            }
             if (res.data.data.match) {
               console.log(res.data.data);
+
               this.props.setMatchmaking(res.data.data.match);
 
             }
@@ -61,7 +71,7 @@ class Participate extends Component {
       return (
         <div>
           <ListMatchmacking />
-          <ListRequest requests={this.props.matchmaking.request} />
+          <ListRequest />
           <ButtonUnParticipate />
         </div>
       );
@@ -78,10 +88,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
+    updateMatchmakingRequest: request => {
+      dispatch(updateMatchmakingRequest(request))
+    },
     setMatchmaking: match => {
       dispatch(setMatchmaking(match))
     }
-
   }
 };
 export default connect(mapStateToProps,mapDispatchToProps)(Participate)
