aea208496983bf30052761e7ace67e0049db7b1b
valentin.montal <valentin.montal4@gmail.com>
2019-01-26 00:25:01 +0100

    Modif deck

--- leagueofstone/src/MakeDeck.js
+++ leagueofstone/src/MakeDeck.js
@@ -1,50 +1,117 @@
 import React, { Component } from "react";
-
 // Redux
 import { connect } from 'react-redux';
 import setMatch from './actions/setMatch';
 
 import './App.css'
-import logo from "./logo.svg";
 import "./Game.css";
-import Match from "./Match";
-import axios from "axios";
-import {SERVER_URL} from "./consts";
+import "./stylesheets/makeDeck.css"
+
+// import axios from "axios";
+// import {SERVER_URL} from "./consts";
+import Card from "./Card";
 
 class MakeDeck extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: []
+            cards: [],
+            deck: [],
+            isLoaded: false,
+            nbCards: 0
         }
     }
-     componentDidMount() {
-         axios
-             .get(
-                 SERVER_URL + "/cards/getAll?token=" +
-                 this.props.sessionToken.token
-             )
-             .then(res => {
-                 if (res.data.status === "ok") {
-                     console.log(res.data.data);
-                     // this.props.history.push(process.env.PUBLIC_URL + "/");
-                 } else {
-                     console.log(res.data.message);
-                 }
-             });
-     }
+
+    componentDidMount() {
+        fetch("champion.json")
+            .then(response => response.json())
+            .then(response => {
+                // tab = []
+                // for(champion in response) {
+                //     cards = {
+                //         id: champion.id,
+                //         key: champion.key,
+                //         name: champion.name,
+                //         title: champion.title,
+                //         image: champion.image,
+                //         skins: champion.skins,
+                //         info: champion.info,
+                //
+                //     }
+                // }
+                this.setState({
+                    isLoaded: true,
+                    cards: response
+                });
+            }).catch(error =>
+            console.error("Error : chooseChampions : ", error)
+        );
+
+        // axios
+        //     .get(
+        //         SERVER_URL + "/cards/getAll?token=" +
+        //         this.props.sessionToken.token
+        //     )
+        //     .then(res => {
+        //         if (res.data.status === "ok") {
+        //             console.log(res.data.data);
+        //             // this.props.history.push(process.env.PUBLIC_URL + "/");
+        //         } else {
+        //             console.log(res.data.message);
+        //         }
+        //     });
+    }
+
+    handleClickToDeck(index) {
+        if(this.state.nbCards < 20) {
+            const champion = this.state.cards[index];
+            this.setState(state => ({
+                deck: [...state.deck, champion],
+                cards: state.cards
+                    .filter((val, i) => i !== index),
+                nbCards: state.nbCards + 1,
+            }));
+        }
+    }
+
+    handleClickToCards(index) {
+        const champion = this.state.deck[index];
+
+        this.setState(state => ({
+            cards: [...state.cards, champion],
+            deck: state.deck
+                .filter((val, i) => i !== index),
+            nbCards: state.nbCards - 1,
+        }));
+    }
 
     render() {
-       return (
-           <div className="Board card-columns">
-               {/*{this.state.champions.map((champ, index) =>*/}
-                   {/*<Card key={index} name={champ.name} img={champ.img}*/}
-                         {/*flipped={champ.flipped}*/}
-                         {/*onClick={() => this.handleClick(index,champ.id,champ.flipped)} />*/}
-               {/*)}*/}
-           </div>
-       )
-
+        if(this.state.isLoaded) {
+            return (
+                <div className="MakeDeck">
+                    <div className="TabCardsToAdd">
+                        <p>Cartes du deck</p>
+                        {this.state.deck.map((champ, index) =>
+                            <Card key={index} name={champ.name} img={champ.name}
+                                // choosed={champ.flipped}
+                                  onClick={() => this.handleClickToCards(index)}/>
+                        )}
+                    </div>
+                    <div className="Board card-columns TabCardsToAdd">
+                        <p>Tab de carte a choisir</p>
+                        {this.state.cards.map((champ, index) =>
+                            <Card key={index} name={champ.name} img={champ.name}
+                                // choosed={champ.flipped}
+                                  onClick={() => this.handleClickToDeck(index)}/>
+                        )}
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div><p>Loading...</p></div>
+                )
+        }
     }
 }
 
