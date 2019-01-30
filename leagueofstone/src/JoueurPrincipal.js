4e7157232822a2f75fbe82e1b9cecfac44d69f43
valentin.montal <valentin.montal4@gmail.com>
2019-01-30 23:37:51 +0100

    Reparation joueur principal

--- leagueofstone/src/JoueurPrincipal.js
+++ leagueofstone/src/JoueurPrincipal.js
@@ -12,7 +12,7 @@ class JoueurPrincipal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            hands: []
+            hand: []
         }
     }
     
@@ -23,29 +23,35 @@ class JoueurPrincipal extends Component {
               this.props.sessionToken.token
        )
           .then(res => {
-            console.log(res);
-              if (res.statusText === "OK") {
-                  let finalHands = [];
-                  let handsRequete = res.data.data.player1.hand
-                  for (let cartes in handsRequete){
-                    console.log("toto");
+            console.log(res.data);
+              if (res.data.status === "ok") {
+                  // let finalHands = [];
+                  // let handsRequete = res.data.data.player1.hand
+                  // console.log(handsRequete)
+                  this.setState({
+                      hand: res.data.data.player1.hand
+                  })
 
-                    finalHands.push(
-                        <Card key={res.data.data.player1.hand.index} onClick={null} name={res.data.data.player1.hand.name} img={res.data.data.player1.hand.key}  info={res.data.data.player1.hand.stats} />
-                    )
-                    }
-              this.setState({
-                  hands: finalHands,
-              })     
-                    console.log(this.state.hands);
+                  // for (let cartes in handsRequete){
+                  //   console.log("toto");
+                  //
+                  //   finalHands.push(
+                  //       <Card key={cartes.key} onClick={null} name={cartes.name} img={cartes.key}  info={cartes.stats} />
+                  //   )
+                  //   }
+              // this.setState({
+              //     hands: finalHands,
+              // })
+              //       console.log(this.state.hand);
 
             } else {
-                  console.log(this.state.hands);
+                  console.log(this.state.hand);
               }
          });
     }
   
     render() {
+        console.log(this.state.hand)
       return (
         <div class="container">
             <div class="div_img">
@@ -53,12 +59,14 @@ class JoueurPrincipal extends Component {
                 Carte visible
             </div>
             <div class="div_img">
-                {this.state.hands}
-                {/* .map((card, index) => {
+                {this.state.hand.map((card, index) => {
                     {console.log(card)}
-                        return (<Card key={index} onClick={null} name={card.name} img={card.key}  info={card.stats} />)
-                    })} */}
-
+                        return (<Card key={index}
+                                      onClick={null}
+                                      name={card.name}
+                                      img={card.key}
+                                      info={card.stats} />)
+                    })}
             </div>
             <div class="row">
                 <div class="col">
