f3c99f9c27132e09a883b48e988560c8e404cbfc
valentin.montal <valentin.montal@mathinfo.i-univ-tlse2.fr>
2019-02-01 10:29:19 +0100

    Revert "Merge branch 'devKamal' into 'dev'"
    This reverts merge request !18

--- leagueofstone/src/JoueurAdverse.js
+++ leagueofstone/src/JoueurAdverse.js
@@ -12,20 +12,26 @@ class JoueurAdverse extends Component {
 
     render() {
       return (
-          <div>
-            <div className="hero">
+        <div className="container">
+            <div className="row">
                 <div className="col">
+                    <img src="/lol.jpg"/>
                 </div>
-                <div className="col1">
-                    <p>{console.log(this.props.player.deck)}
-                    {this.props.player.deck}</p>
-                </div>
-            </div>
-            <div className="visible">
-                Carte Visible
-            </div>
-            </div>
+                <div className="deck">
             
+                {console.log(this.props.player.deck)}
+                    {this.props.player.deck}
+                </div>
+            </div>
+            <div className="div_img">
+                {/* <CardVisible /> */}
+                Carte Visible
+
+            </div>
+            <div className="hand">
+              {<HandsCards handPlayer={this.props.player.hand}/>}
+            </div>
+        </div>
       );
     }
 
