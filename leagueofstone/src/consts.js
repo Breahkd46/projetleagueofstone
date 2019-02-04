c614876dd19bbacb75208ca9aa81d6761a1003a5
valentin.montal <valentin.montal4@gmail.com>
2019-02-05 00:22:32 +0100

    TEST build

--- leagueofstone/src/consts.js
+++ leagueofstone/src/consts.js
@@ -1,4 +1,5 @@
-const SERVER_URL = "http://localhost:3001";
+// const SERVER_URL = "http://localhost:3001";
+const SERVER_URL = "http://ling.westeurope.cloudapp.azure.com/";
 
 const RELOAD_TIME = 5000;
 
