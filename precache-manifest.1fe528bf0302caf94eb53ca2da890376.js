3aae30b67d2633a0405727096e66545f72436c26
valentin.montal <valentin.montal4@gmail.com>
2019-02-05 00:37:28 +0100

    Updates

---
+++ precache-manifest.1fe528bf0302caf94eb53ca2da890376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c30e704c8fd329ee7859",
+    "url": "/projetleagueofstone/static/css/main.69c18c36.chunk.css"
+  },
+  {
+    "revision": "c30e704c8fd329ee7859",
+    "url": "/projetleagueofstone/static/js/main.c30e704c.chunk.js"
+  },
+  {
+    "revision": "a6941998568bde771d23",
+    "url": "/projetleagueofstone/static/js/1.a6941998.chunk.js"
+  },
+  {
+    "revision": "170a11d017afa4fbb4a0",
+    "url": "/projetleagueofstone/static/js/runtime~main.170a11d0.js"
+  },
+  {
+    "revision": "549fdb3e48fa6450dc28a702455189d9",
+    "url": "/projetleagueofstone/index.html"
+  }
+];
