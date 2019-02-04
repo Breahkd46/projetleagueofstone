ee3ff37653994af1d850c0192eebb0dc9c7128df
valentin.montal <valentin.montal4@gmail.com>
2019-02-05 00:24:13 +0100

    test build

---
+++ docs/precache-manifest.e2fccca4fd7e8b3ceaace4bb5cdf74a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d7e49787deb7e9e3fd",
+    "url": "/static/css/main.69c18c36.chunk.css"
+  },
+  {
+    "revision": "c6d7e49787deb7e9e3fd",
+    "url": "/static/js/main.c6d7e497.chunk.js"
+  },
+  {
+    "revision": "a6941998568bde771d23",
+    "url": "/static/js/1.a6941998.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "417be68af0652933edbe4fe1b2fdf855",
+    "url": "/index.html"
+  }
+];
